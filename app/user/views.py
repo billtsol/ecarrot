fe10f658db672978ffe1fb7d1ed32146cdfb54ae
billtsol <billtsol@protonmail.com>
2024-04-27 00:22:11 +0300

    Implement manage user API and tests

--- app/user/views.py
+++ app/user/views.py
@@ -2,7 +2,11 @@
 Views for the user API
 """
 
-from rest_framework import generics # type: ignore
+from rest_framework import ( # type: ignore
+  generics,
+  authentication,
+  permissions
+)
 from rest_framework.authtoken.views import ObtainAuthToken # type: ignore
 from rest_framework.settings import api_settings # type: ignore
 from user.serializers import (
@@ -19,4 +23,12 @@ class CreateTokenView(ObtainAuthToken):
   serializer_class = AuthTokenSerializer
   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
-  
+class ManageUserView(generics.RetrieveUpdateAPIView):
+  """Manage the authenticated user"""
+  serializer_class = UserSerializer
+  authentication_classes = [authentication.TokenAuthentication]
+  permission_classes = [permissions.IsAuthenticated]
+
+  def get_object(self):
+    """Retrieve and return authenticated user"""
+    return self.request.user
