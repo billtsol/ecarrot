41b13b682aaf56f61741bf14bb6f6fdd310f87b9
BillTsol <billtsol1@gmail.com>
2024-12-18 16:05:22 +0100

    comments

--- backend/api/user/views.py
+++ backend/api/user/views.py
@@ -18,11 +18,22 @@ class CreateUserView(generics.CreateAPIView):
   """Create a new user in the system"""
   serializer_class = UserSerializer
 
+'''
+ObtainAuthToken is a class-based view in Django REST Framework (DRF) 
+that provides an endpoint for obtaining an authentication token. 
+It allows users to authenticate via their credentials (usually 
+username and password) and receive a token, 
+which can then be used for subsequent authenticated requests'''
 class CreateTokenView(ObtainAuthToken):
   """Create a new auth token for user"""
   serializer_class = AuthTokenSerializer
   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
+'''
+The generics.RetrieveUpdateAPIView is a class in Django REST Framework
+that provides functionality for retrieving and updating an object, 
+handling GET and PUT or PATCH requests automatically.
+'''
 class ManageUserView(generics.RetrieveUpdateAPIView):
   """Manage the authenticated user"""
   serializer_class = UserSerializer
